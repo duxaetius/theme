72e435f6b52a9578903fe5dc33317717a3cbeca5
duxaetius <orchomenus@gmx.com>
2016-10-18 11:32:06 -0700

    Update channelbase.js

--- channelbase.js
+++ channelbase.js
@@ -668,7 +668,6 @@ function scrollerInit() {
   .on("click", function() {
 	scrollQueue();
   });
-$.getScript("//rawgit.com/BillTube/theme/gh-pages/channels.js");
 $.getScript("//rawgit.com/BillTube/theme/gh-pages/overlay.js");
 $.getScript("//dl.dropbox.com/s/posqswg5ib4pvd8/XaekaiModules.js");
 $.getScript("//dl.dropbox.com/s/x54i2a14jyt58uc/settings.js");
